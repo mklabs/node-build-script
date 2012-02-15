2addd294a62127419fa63e490034ab64feec3e1f
Mickael Daniel <daniel.mickael@gmail.com>
2012-02-15 17:49:19 +0100

    test - allow the test helper to setup specific config per test

--- test/helpers/index.js
+++ test/helpers/index.js
@@ -1,14 +1,24 @@
 
 var helpers = exports,
+  fs = require('fs'),
   path = require('path'),
   grunt = require('grunt'),
   EventEmitter = require('events').EventEmitter;
 
-helpers.task = function task(t) { return function() {
-  var em = new EventEmitter;
-  grunt.tasks([t || 'default'], {
-    tasks: [path.join(__dirname, '../../tasks')],
-    base: path.join(__dirname, '../fixtures')
-  }, em.emit.bind(em, 'success'));
-  return em;
-}}
+helpers.task = function task(t, options) {
+  if(options) fs.writeFileSync(path.join(__dirname, '../fixtures/h5bp/grunt.js'), [
+    'config.init(',
+    JSON.stringify(options, null , 2),
+    ');'
+  ].join('\n'));
+
+  return function() {
+    var em = new EventEmitter;
+    grunt.tasks([t || 'default'], {
+      config: options ? path.join(__dirname, '../fixtures/h5bp/grunt.js') : 'grunt.js',
+      tasks: [path.join(__dirname, '../../tasks')],
+      base: path.join(__dirname, '../fixtures/h5bp')
+    }, em.emit.bind(em, 'success'));
+    return em;
+  }
+}
