e93937998d6d556def721acc9a41dc8b753e19cb
Mickael Daniel <daniel.mickael@gmail.com>
2012-02-22 20:11:22 +0100

    usemin - use the new config('output') value
    This closes #3

--- tasks/usemin.js
+++ tasks/usemin.js
@@ -3,10 +3,26 @@ var fs = require('fs'),
   path = require('path');
 
 //
-// ### Tasks
+// ### Usemin Task
 //
+// Replaces references ton non-optimized scripts / stylesheets into a
+// set of html files (or any template / views).
+//
+// Right now the replacement is based on the filename parsed from
+// content and the files present in accoding dir (eg. looking up
+// matching revved filename into `output/` dir to know the sha
+// generated).
+//
+// Todo: Use a file dictionary during build process and rev task to
+// store each optimized assets and their associated sha1.
+//
+
 task.registerBasicTask('usemin', 'Replaces references to non-minified scripts / stylesheets', function(data, name) {
-  var files = file.expand(data);
+  var files = file.expand(data),
+    output = path.resolve(config('output')),
+    cssdir = path.join(output, 'css'),
+    imgdir = path.join(output, 'img'),
+    jsdir = path.join(output, 'js');
 
   files.map(file.read).forEach(function(content, i) {
     var p = files[i];
@@ -17,7 +33,7 @@ task.registerBasicTask('usemin', 'Replaces references to non-minified scripts / 
     log.writeln('Update the HTML to reference our concat/min/revved script file');
     content = content.replace(/<!--\s*scripts concatenated[\d\w\s\W\n]*<!--\s*end scripts\s*-->/gm, function(match, prefix) {
       // hmm not that configurable, think of another way, but that'll be good for now
-      var file = fs.readdirSync(path.resolve('publish/js')).filter(function(f) {
+      var file = fs.readdirSync(jsdir).filter(function(f) {
         return path.basename(f).split('.').slice(1).join('.') === 'scripts.js';
       })[0];
 
@@ -30,7 +46,7 @@ task.registerBasicTask('usemin', 'Replaces references to non-minified scripts / 
     log.writeln('Update the HTML with the new css filename');
     content = content.replace(/<link rel=["']?stylesheet["']?\shref=["']?(.*)\/style.css["']?\s*>/gm, function(match, prefix) {
       // same here
-      var file = fs.readdirSync(path.resolve('publish/css')).filter(function(f) {
+      var file = fs.readdirSync(cssdir).filter(function(f) {
         return path.basename(f).split('.').slice(1).join('.') === 'style.css';
       })[0];
 
@@ -40,7 +56,7 @@ task.registerBasicTask('usemin', 'Replaces references to non-minified scripts / 
     log.writeln('Update the HTML with the new img filename');
     content = content.replace(/<img.+src=['"](.+)\/([^\/]+)["'][^>]*>/, function(match, prefix, src) {
       // same here
-      var file = fs.readdirSync(path.resolve('publish/img')).filter(function(f) {
+      var file = fs.readdirSync(imgdir).filter(function(f) {
         return path.basename(f).split('.').slice(1).join('.') === src;
       })[0];
 
