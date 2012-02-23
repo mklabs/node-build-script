decf1b988a4c96951e52aecc55e4015e982b0cbc
krzychukula <krzychukula@gmail.com>
2012-02-23 22:21:43 +0100

    if script exists then rev it!

--- tasks/usemin.js
+++ tasks/usemin.js
@@ -14,17 +14,21 @@ task.registerBasicTask('usemin', 'Replaces references to non-minified scripts / 
     log.writeln('switch from a regular jquery to minified');
 
 
-    log.writeln('Update the HTML to reference our concat/min/revved script file');
-    content = content.replace(/<!--\s*scripts concatenated[\d\w\s\W\n]*<!--\s*end scripts\s*-->/gm, function(match, prefix) {
-      // hmm not that configurable, think of another way, but that'll be good for now
-      var file = fs.readdirSync(path.resolve('publish/js')).filter(function(f) {
-        return path.basename(f).split('.').slice(1).join('.') === 'scripts.js';
-      })[0];
+    log.writeln('Update the HTML to reference our concat/min/revved script files');
+    content = content.replace(/<script.+src=['"](.+)\/([^\/"']+)["'][\/>]?><\/script>/mg, function(match, prefix, src) {
 
-      // guess the relative prefix path from one of the script, needs rework obviously
-      prefix = match.match(/<script.+src=["'](.*)\/script.js["']\s*><\/script>/);
-      prefix = (prefix && prefix[1]) || '';
-      return '<script defer src=":file"></script>'.replace(':file', [prefix, file].join('/'));
+      if(prefix.match(/\/\//)){
+        //do not touch external files
+        return match;
+      }
+      var fileName = file.expand('publish/js/**/*' + src)[0];
+      if(fileName){
+        fileName = fileName.replace('publish', '');
+      return '<script defer src=":file"></script>'.replace(':file', fileName);
+      }
+      //if file not exists probaly was concatenated into another file so skip it
+      return '';
+
     });
 
     log.writeln('Update the HTML with the new css filename');
