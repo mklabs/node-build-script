d0268b8f5457ffd10029c4b9aa09967397ad9ba4
Mickael Daniel <daniel.mickael@gmail.com>
2012-03-28 23:56:16 +0200

    test - reworked the runner, setting up spawned grunt
    * commands run serially
    * tests fail or pass depending on grunt's exit code

---
+++ test/helpers/index.js
@@ -0,0 +1,42 @@
+var assert = require('assert'),
+  resolve = require('path').resolve,
+  spawn = require('child_process').spawn;
+
+//
+// Grunt runner helper. A stack of command is built when called, to finally run
+// them sequentially on the next event loop.
+//
+// Depending on grunt's exit code, the test fails or pass.
+//
+module.exports = function grunt(base) {
+  base = base || 'test/h5bp';
+
+  return function(cmd) {
+    var stack = grunt.stack || (grunt.stack = []);
+    stack.push(cmd);
+
+    process.nextTick(function() {
+      if(grunt.started) return;
+      grunt.started = true;
+
+      // path to the grunt executable, going to node dependency but could be done
+      // with the global grunt instead
+      var gruntpath = resolve('node_modules/grunt/bin/grunt');
+
+      // now that the stack is setup, run each command serially
+      (function run(cmd) {
+        if(!cmd) return;
+        // grunt process
+        var gpr = spawn(gruntpath, cmd.split(' '), { cwd: resolve(base) });
+        gpr.stdout.pipe(process.stdout);
+        gpr.stderr.pipe(process.stderr);
+
+        gpr.on('exit', function(code) {
+          assert.equal(code, 0, ' ✗ Grunt exited with errors. Code: ' + code);
+          run(stack.shift());
+        });
+      })(stack.shift());
+    });
+  }
+};
+
