e9b6644e3c02a21cefbd1daea02e096be620ef01
Mickael Daniel <daniel.mickael@gmail.com>
2012-03-30 13:11:18 +0200

    test - tweak the test runner to be usable on windows

--- test/helpers/index.js
+++ test/helpers/index.js
@@ -1,6 +1,6 @@
 var assert = require('assert'),
   resolve = require('path').resolve,
-  spawn = require('child_process').spawn;
+  exec = require('child_process').exec;
 
 //
 // Grunt runner helper. A stack of command is built when called, to finally run
@@ -26,12 +26,16 @@ module.exports = function grunt(base, fn) {
       // now that the stack is setup, run each command serially
       (function run(cmd) {
         if(!cmd) return;
-        // grunt process
-        var gpr = spawn(gruntpath, cmd.split(' '), { cwd: resolve(base) });
+        // grunt process - maybe fork would avoid the exec use for this to work on windows, I dunno
+        console.log('running in ', resolve(base));
+        console.log(' » grunt ' + cmd);
+        console.log();
+
+        var gpr = exec('node ' + gruntpath + ' ' + cmd, { cwd: resolve(base) });
         gpr.stdout.pipe(process.stdout);
         gpr.stderr.pipe(process.stderr);
-
-        gpr.on('exit', function(code) {
+        gpr.on('exit', function(code, stdout, stderr) {
+          console.log(arguments);
           assert.equal(code, 0, ' ✗ Grunt exited with errors. Code: ' + code);
           run(stack.shift());
         });
